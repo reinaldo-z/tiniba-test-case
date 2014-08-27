8bb9dcd52b66579c3107ec64eee80e98c9d5a711
Sean Anderson <sean.martin.anderson@gmail.com>
2014-08-27 17:09:13 -0500

    fixed typo in control.sh

--- control.sh
+++ control.sh
@@ -14,7 +14,7 @@ if [[ "$mode" == "run" ]]; then
     run_tiniba.sh -r run -k 19 -N half-slab -x 2 -p
     run_tiniba.sh -r run -k 19 -N half-slab -x 2 -c
     all_responses.sh -w total -m 19_5-nospin -s 0 -o 1 -v 13 -c 17 -r 1 -t "xx"
-elif [[ "$mode" == "clean"]]; then
+elif [[ "$mode" == "clean" ]]; then
     run_tiniba.sh -r erase
     run_tiniba.sh -r erasescf
     rm -rf .ab_layers.d .acell.d .back.layers back.layers.xy eigen_* .fnval .front.layers front.layers.xy half_slab.* .i* layers.d* .lista_layers logfiles/ .machines_pmn .machines_scf .machines_latm .machines_res me_* mtita .peso* si_as_6_* *.klist* si_as_6.xyz.original spin_info symmetries/ .trueMachines
