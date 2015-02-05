21e16b716ee93f556d749d2b4fb2099b75201978
Sean Anderson <sean.martin.anderson@gmail.com>
2015-02-04 21:28:58 -0600

    we are done

--- control.sh
+++ control.sh
@@ -12,12 +12,12 @@ if [[ "$mode" == "run" ]]; then
     chose_layers.sh half-slab
     #run_tiniba.sh -r setkp -k 40 -g 2 -G 2 
     run_tiniba.sh -r run -k 40 -N half-slab -x 2 -C 4 -P 200 -w -e -p
-    run_tiniba.sh -r run -k 40 -N half-slab -x 2 -c
+    run_tiniba.sh -r run -k 40 -N half-slab -x 2 -P 200 -c
     all_responses.sh -w total -m 40_5-nospin -s 0.0 -o 1 -v 13 -c 17 -r 1 -t "xx"
     all_responses.sh -w total -m 40_5-nospin -s 0.0 -o 1 -v 13 -c 17 -r 21 -t "xxx"
     all_responses.sh -w layer -m 40_half-slab_5-nospin -s 0.0 -o 1 -v 13 -c 17 -r 44 -t "xxx"
 elif [[ "$mode" == "clean" ]]; then
     run_tiniba.sh -r erase
     run_tiniba.sh -r erasescf
-    rm -rf .ab_layers.d .acell.d .back.layers back.layers.xy eigen_* .fnval .front.layers front.layers.xy half_slab.* hoy* .i* layers.d* .lista_layers logfiles/ .machines_pmn .machines_scf .machines_latm .machines_res me_* mtita opt.dat .peso* si_as_6_* *.klist* si_as_6.xyz.original spin_info symmetries/ .trueMachines
+    rm -rf .ab_layers.d .acell.d .back.layers back.layers.xy .cores eigen_* .fnval .front.layers front.layers.xy half_slab.* hoy* .i* layers.d* .lista_layers logfiles/ .machines_pmn .machines_scf .machines_latm .machines_res me_* mtita opt.dat .peso* si_as_6_* *.klist* si_as_6.xyz.original spin_info symmetries/ tmp* .trueMachines
 fi
